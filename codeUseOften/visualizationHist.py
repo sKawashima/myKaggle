204ae52fa419eafdf4d4f25af9ea2ecae4691b25
sKawashima <bysk.333@gmail.com>
2020-01-26 21:41:10 +0900

    :pencil: Add visualizationPoint

--- codeUseOften/visualizationHist.py
+++ codeUseOften/visualizationHist.py
@@ -5,6 +5,6 @@ g.map(plt.hist, '分析対象', bins=20)
 
 # 2次比較
 
-g = sns.FacetGrid(train_df, col='比較対象', row='分析対象')
+g = sns.FacetGrid(train_df, col='比較対象', row='比較対象')
 g.map(plt.hist, '分析対象', bins=20)
 g.add_legend()
