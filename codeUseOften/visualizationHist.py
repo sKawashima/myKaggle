0e24c6bbc64758782adab73890e7f827b0ea2034
sKawashima <bysk.333@gmail.com>
2020-01-26 21:34:35 +0900

    :pencil: Add 2D plot

--- codeUseOften/visualizationHist.py
+++ codeUseOften/visualizationHist.py
@@ -1,2 +1,10 @@
+# 1次比較
+
 g = sns.FacetGrid(train_df, col='比較対象')
 g.map(plt.hist, '分析対象', bins=20)
+
+# 2次比較
+
+g = sns.FacetGrid(train_df, col='比較対象', row='分析対象')
+g.map(plt.hist, '分析対象', bins=20)
+g.add_legend()
